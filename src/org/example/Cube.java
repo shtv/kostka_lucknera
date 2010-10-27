4c985747512a1170206b46e7a760b9e12009b6c5
pawbar <pwelbrn@gmail.com>
2010-10-28 00:06:38 +0200

    Plik antowy i jedna klasa

---
+++ src/org/example/Cube.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Cube{
+	long x,y,z;
+
+	/**
+	 * Constructs a new instance.
+	 *
+	 * @param x The x for this instance.
+	 * @param y The y for this instance.
+	 * @param z The z for this instance.
+	 */
+	public Cube(long x, long y, long z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+}
