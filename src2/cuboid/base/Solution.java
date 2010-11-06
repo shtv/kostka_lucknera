4d976f915db9a22e0b5d536ebd03151c15287ef5
leguckia <leguckia@gmail.com>
2010-11-06 20:24:59 +0100

    Nowa propozycja struktury klas.

---
+++ src2/cuboid/base/Solution.java
@@ -0,0 +1,8 @@
+package cuboid.base;
+
+import java.util.List;
+
+public class Solution {
+		private List<Move> sequence;
+		private int volume;
+}
---
+++ src2/cuboid/solvers/ExactSolutionFinder.java
@@ -0,0 +1,45 @@
+package cuboid.solvers;
+
+import java.util.List;
+import cuboid.base.*;
+
+
+public class ExactSolutionFinder {
+		
+		private Solution bestSolution;
+		private class ListNode
+		{
+			int n;
+			ListNode next;
+			ListNode prev;
+		}
+	
+		public ExactSolutionFinder(List<Block> blocks)
+		{
+		}
+		
+		private void nextPerm()
+		{
+		}
+		
+		private void checkPerm()
+		{
+		}
+		
+		private void checkSequence()
+		{
+		}
+		
+		private void nextFit()
+		{
+		}
+		
+		public Solution getSolution()
+		{
+			return null;
+		}
+		
+		public static void main(String[] args)
+		{
+		}
+}
