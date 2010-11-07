ffd36b1cdea3e82431d71e2f58d90f9504f8a405
pawbar <pwelbrn@gmail.com>
2010-11-07 22:20:45 +0100

    Dodane pole kodu klocka do jego klasy

--- src/cuboid/base/Block.java
+++ src/cuboid/base/Block.java
@@ -3,27 +3,33 @@ package cuboid.base;
 import java.util.Set;
 
 public class Block {
-		private int maxX;
-		private int maxY;
-		private int maxZ;
-		private int cuboidsCount;
-		Set<Cube> cubes;
-		
-		public int getMaxX()
-		{
-			return maxX;
-		}
+	private int maxX;
+	private int maxY;
+	private int maxZ;
+	private int cuboidsCount;
+	Set<Cube> cubes;
+	/**
+	 * It is a Luckner's code. For example, a simple chair's code would be
+	 * (U)(D)LLU.
+	 */
+	String id;
+	
+	public int getMaxX()
+	{
+		return maxX;
+	}
 
-		public int getMaxY() {
-			return maxY;
-		}
 
-		public int getMaxZ() {
-			return maxZ;
-		}
-		
-		public int getCuboidsCount()
-		{
-			return cuboidsCount;
-		}
+	public int getMaxY() {
+		return maxY;
+	}
+
+	public int getMaxZ() {
+		return maxZ;
+	}
+	
+	public int getCuboidsCount()
+	{
+		return cuboidsCount;
+	}
 }
--- src/cuboid/solvers/CloningAlgorithm.java
+++ src/cuboid/solvers/CloningAlgorithm.java
@@ -1,5 +1,6 @@
 package cuboid.solvers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cuboid.base.BlockCollection;
@@ -8,7 +9,14 @@ import cuboid.base.Solution;
 public class CloningAlgorithm implements SolutionFinder {
 
 	public Solution getSolution(List<BlockCollection> blockCollections) {
+		List<BlockCollection> blocks = new
+			ArrayList<BlockCollection>();
 
+		for(BlockCollection blockCollection : blockCollections)
+			if(blockCollection.getAmount()>1)
+				blocks.add(blockCollection);
+
+		if(blocks.isEmpty()) return null;
 
 		return null;
 	};
